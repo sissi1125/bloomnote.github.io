380f8f0dde0762415f5a1528c44fe047e373efae
sissi <melon.xyxy@gmail.com>
2025-01-02 02:01:06 +0800

    feat: readme

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   reactStrictMode: false,
   output: 'export',
   images: { unoptimized: true },
+  assetPrefix: '/out',
   // i18n: {
   //   locales: ['zh-CN', 'en'],
   //   defaultLocale: 'zh-CN', // 默认语言
